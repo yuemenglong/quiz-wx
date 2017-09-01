b947d36655475d67511b0f9e987f9be4fcecb04c
yuemenglong <yuemenglong@126.com>
2017-09-01 10:20:20 +0800

    add: 完成quiz的继续

--- pages/index/index.ts
+++ pages/index/index.ts
@@ -2,91 +2,66 @@ import store = require("../../reducer/store");
 import ActionCreator = require("../../reducer/action-creator");
 import wxx = require("../../kit/wxx");
 import User = require("../../common/entity/user");
+import State = require("../../common/state/state");
 
 //noinspection JSUnusedGlobalSymbols
 /**
  * Created by <yuemenglong@126.com> on 2017/7/27
  */
 
-interface IndexData {
+class IndexData {
     user: User;
+    current: string;
 }
 
 class IndexClass {
     data: IndexData;
 
-    currentType() {
-        if (this.data.user.study) {
+    // noinspection JSMethodCanBeStatic
+    currentType(state: State): string {
+        if (!state.user) {
+            return ""
+        }
+        if (state.user.study) {
             return "study"
-        } else if (this.data.user.quiz) {
+        } else if (state.user.quiz) {
             return "quiz"
-        } else if (this.data.user.marked) {
+        } else if (state.user.marked) {
             return "marked"
         }
     }
 
-    needTip(type) {
-        let current = this.currentType();
-        if (current == type || current == null) {
-            return false;
+    // noinspection JSMethodCanBeStatic
+    stopCurrent(): Promise<boolean> {
+        function tip(): Promise<boolean> {
+            let state = store.getState();
+            if (state.user.study) {
+                return wxx.showModal("提示", "您还有未完成的学习，是否忽略")
+            } else if (state.user.quiz) {
+                return wxx.showModal("提示", "您还有未完成的模拟测试，是否忽略")
+            } else if (state.user.marked) {
+                return wxx.showModal("提示", "您还有未完成的易错题回顾，是否忽略")
+            } else {
+                return new Promise((resolve) => {
+                    resolve(true)
+                })
+            }
         }
-        switch (current) {
-            case "study":
-                //noinspection JSIgnoredPromiseFromCall
-                wxx.showTip("提示", "您当前在学习模式下还有未完成的题目，请前往学习模式");
-                break;
-            case "quiz":
-                //noinspection JSIgnoredPromiseFromCall
-                wxx.showTip("提示", "您当前在测验模式下还有未完成的题目，请前往测验模式");
-                break;
-            case "marked":
-                //noinspection JSIgnoredPromiseFromCall
-                wxx.showTip("提示", "您当前在模拟考试模式下还有未完成的题目，请前往模拟考试模式");
-                break;
-        }
-        return true;
+
+        return tip().then(choose => {
+            if (choose) {
+                store.dispatch(ActionCreator.setQuizData({answer: ""}));
+            }
+            return choose;
+        })
     }
 
     //noinspection JSUnusedGlobalSymbols
-    // bindQuiz() {
-    // if (this.needTip("quiz")) {
-    //     return;
-    // }
-    // // 找到一个没有做完的quiz
-    // let quiz = store.getState().currentQuiz();
-    // if (quiz == null) {
-    //     // 没有则建立新quiz
-    //     return store.dispatch(ActionCreator.newQuiz(quiz => {
-    //         store.dispatch(ActionCreator.setGlobalData({inStudy: false, quizId: quiz.id}));
-    //         return wxx.navigateTo(`../quiz/quiz`)
-    //     }))
-    // } else if (quiz.questions.length == 0) {
-    //     // 拿到quiz详情
-    //     return store.dispatch(ActionCreator.fetchQuiz(quiz.id, (quiz) => {
-    //         store.dispatch(ActionCreator.setGlobalData({inStudy: false, quizId: quiz.id}));
-    //         return wxx.navigateTo(`../quiz/quiz`)
-    //     }))
-    // } else {
-    //     // quiz存在且有详情信息
-    //     store.dispatch(ActionCreator.setGlobalData({inStudy: false, quizId: quiz.id}));
-    //     return wxx.navigateTo(`../quiz/quiz`)
-    // }
-    // }
-
-    //noinspection JSUnusedGlobalSymbols
     bindNewStudy() {
-        console.log("bindNewStudy");
-        let state = store.getState();
-        if (state.user.study != null) {
-            wxx.showModal("提示", "您还有未完成的学习，是否忽略并重新开始新的学习").then((choose) => {
-                if (!choose) return;
-                store.dispatch(ActionCreator.deleteQuiz(state.user.study, () => {
-                    return wxx.navigateTo(`../study/study-chapter`);
-                }))
-            })
-        } else {
+        this.stopCurrent().then((choose) => {
+            if (!choose) return;
             return wxx.navigateTo(`../study/study-chapter`);
-        }
+        });
     }
 
     //noinspection JSMethodCanBeStatic,JSUnusedGlobalSymbols
@@ -104,29 +79,27 @@ class IndexClass {
 
     //noinspection JSUnusedGlobalSymbols
     bindNewQuiz() {
-        // if (this.needTip("quiz")) {
-        //     return;
-        // }
-        store.dispatch(ActionCreator.newQuiz(() => {
-            return wxx.navigateTo(`../quiz/quiz-answer`);
-        }))
-        // store.dispatch(ActionCreator.setGlobalData({inStudy: false}));
-        // let quiz = store.getState().currentQuiz();
-        // if (quiz) {
-        //     store.dispatch(ActionCreator.setGlobalData({quizId: quiz.id}));
-        // }
-        // if (quiz && quiz.questions.length) {
-        //     return wxx.navigateTo(`../quiz/quiz`)
-        // } else if (quiz && !quiz.questions.length) {
-        //     return store.dispatch(ActionCreator.fetchQuiz(quiz.id, () => {
-        //         return wxx.navigateTo(`../quiz/quiz`)
-        //     }))
-        // } else {
-        //     return store.dispatch(ActionCreator.newExamQuiz(quiz => {
-        //         store.dispatch(ActionCreator.setGlobalData({quizId: quiz.id}));
-        //         return wxx.navigateTo(`../quiz/quiz`)
-        //     }))
-        // }
+        this.stopCurrent().then((choose) => {
+            if (!choose) return;
+            return store.dispatch(ActionCreator.newQuiz(() => {
+                return wxx.navigateTo(`../quiz/quiz-answer`);
+            }))
+        });
+    }
+
+    //noinspection JSMethodCanBeStatic,JSUnusedGlobalSymbols
+    bindContQuiz() {
+        let state = store.getState();
+        let quiz = state.user.quiz;
+        if (quiz.mode == "answer") {
+            wxx.navigateTo(`../quiz/quiz-answer`)
+        } else if (quiz.mode == "review") {
+            wxx.navigateTo(`../quiz/quiz-review`)
+        } else if (quiz.mode == "redo") {
+            wxx.navigateTo(`../quiz/quiz-redo`)
+        } else {
+            throw new Error("Invalid Mode: " + quiz.mode)
+        }
     }
 
     //noinspection JSMethodCanBeStatic,JSUnusedGlobalSymbols
@@ -137,8 +110,10 @@ class IndexClass {
     //noinspection JSUnusedGlobalSymbols
     onShow() {
         store.connect(this, (state) => {
-            let user = state.user;
-            return {user: user}
+            let data = new IndexData();
+            data.user = state.user;
+            data.current = this.currentType(state);
+            return data;
         });
     }
 
@@ -160,4 +135,8 @@ class IndexClass {
     }
 }
 
-Page(new IndexClass());
+Page(
+    new
+
+    IndexClass()
+);
